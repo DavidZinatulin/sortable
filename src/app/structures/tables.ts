5f6e371ed59610d7fa22217d71e4527a81f52d58
davznt <d.zinatulin@ba.lt>
2018-11-26 16:25:00 +0200

    revert table structure

--- src/app/structures/tables.ts
+++ src/app/structures/tables.ts
@@ -4,7 +4,7 @@ export const RecordsTable = [
   {id: 'artist', label: records.artist, sortable: true},
   {id: 'title', label: records.title, sortable: true},
   {id: 'new', label: records.new, sortable: false, format: 'replace-boolean'},
-  {id: 'streams', label: records.streams, sortable: true, format: 'signum'},
+  {id: 'streams', label: records.streams, sortable: true, format: 'round'},
   {id: 'progress', label: records.progress, sortable: true, format: 'signum'},
   {id: 'link', label: records.link, sortable: false, format: 'link'},
 ];
