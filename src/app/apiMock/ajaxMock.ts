6d503c0555040658d94dda5e64269cb09af30bbb
DavidZinatulin <david.zinatulin@synergium.eu>
2018-11-27 00:04:56 +0200

    fixes

--- src/app/apiMock/ajaxMock.ts
+++ src/app/apiMock/ajaxMock.ts
@@ -9,7 +9,7 @@ export const ajaxMock = async (
   previouslyLoaded?: number
 ) => {
 
-  let responseData = sampleData
+  const responseData = sampleData
     .sort(compareValues(sortBy, asc))
     .slice(previouslyLoaded, itemsToLoad);
 
